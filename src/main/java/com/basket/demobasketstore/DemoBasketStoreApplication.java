c45f159470ad9d6caabf6caa53f343a0fe1ee408
datigrov <denistigrov@icloud.com>
2024-02-23 16:52:58 +0300

    first step

---
+++ src/main/java/com/basket/demobasketstore/DemoBasketStoreApplication.java
@@ -0,0 +1,13 @@
+package com.basket.demobasketstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBasketStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBasketStoreApplication.class, args);
+	}
+
+}
